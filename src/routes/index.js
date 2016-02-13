70927ab5d52f799601202b5ef0637af27b0e034b
Arnaud Rebts <arnaud.rebts@gmail.com>
2016-02-13 13:20:29 +0100

    refactor(sample): kill sample code

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, IndexRoute, Redirect } from 'react-router'
+import { Route, Redirect } from 'react-router'
 
 // NOTE: here we're making use of the `resolve.root` configuration
 // option in webpack, which allows us to specify import paths as if
@@ -7,12 +7,10 @@ import { Route, IndexRoute, Redirect } from 'react-router'
 // very easy to navigate to files regardless of how deeply nested
 // your current file is.
 import CoreLayout from 'layouts/CoreLayout/CoreLayout'
-import HomeView from 'views/HomeView/HomeView'
 import NotFoundView from 'views/NotFoundView/NotFoundView'
 
 export default (
   <Route path='/' component={CoreLayout}>
-    <IndexRoute component={HomeView} />
     <Route path='/404' component={NotFoundView} />
     <Redirect from='*' to='/404' />
   </Route>
